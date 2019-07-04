c605085c1802fe8a83a8e680460076c0a5f0ba4c
MinjaeLyou <lminjae0313@gmail.com>
2019-07-05 02:43:38 +0900

    [ADD] New router to add user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,9 +4,24 @@ const wrap = require('express-async-wrap');
 const models = require('../models');
 
 /* GET users listing. */
+// Get all users
 router.get('/', wrap(async (req, res) => {
   const users = await models.user.findAll();
   res.send(users);
+}));
+
+// Create new user
+router.post('/addUser', wrap(async (req, res) => {
+  const user = await models.user.create(req.body);
+  if (user) {
+    res.send ({
+      result: true
+    });
+  } else {
+    res.send({
+      result: false
+    });
+  }
 }));
 
 module.exports = router;
