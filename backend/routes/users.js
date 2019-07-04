fd32e8c29c640893f4fe6f0b84f3c41f3ffe3bf9
MinjaeLyou <lminjae0313@gmail.com>
2019-07-05 02:00:31 +0900

    [ADD] User router and async module

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const wrap = require('express-async-wrap');
+const models = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', wrap(async (req, res) => {
+  const users = await models.user.findAll();
+  res.send(users);
+}));
 
 module.exports = router;
