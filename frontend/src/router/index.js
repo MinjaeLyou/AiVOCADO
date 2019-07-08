b27f06b175d1eb4c31c1eac3da968766bb8aba33
subsevy <49854525+subsevy@users.noreply.github.com>
2019-07-08 22:08:15 +0900

    Sub's Failure
    So sad

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,34 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import IndexPage from '@/components/IndexPage'
-import ShowPage from '@/components/showPage'
-import RegisterPage from '@/components/Register'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import IndexPage from "@/components/IndexPage";
+import ShowPage from "@/components/showPage";
+import RegisterPage from "@/components/Register";
+import sub from "@/components/sub";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'index',
+      path: "/",
+      name: "index",
       component: IndexPage
     },
     {
-      path: '/show/:id',
-      name: 'show',
+      path: "/show/:id",
+      name: "show",
       component: ShowPage
     },
     {
-      path: '/reg',
-      name: 'register',
+      path: "/reg",
+      name: "register",
       component: RegisterPage
     },
+    {
+      path: "/sub",
+      name: "subb",
+      component: sub
+    }
   ]
-})
+});
