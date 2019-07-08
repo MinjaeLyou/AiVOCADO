1d55505642656ed0e330f3312053fb5a415b041f
subsevy <49854525+subsevy@users.noreply.github.com>
2019-07-08 22:17:27 +0900

    Sub's Failure
    Sooo sad

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -32,6 +32,11 @@ export default new Router({
       path: "/",
       name: "start",
       component: StartPage
+    },
+    {
+      path: "/sub",
+      name: "subsub",
+      component: Sub
     }
   ]
 });
