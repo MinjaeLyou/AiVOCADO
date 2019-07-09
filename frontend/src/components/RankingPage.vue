9d6bc28f1076d73c2eb96c116b36d9317b356d60
kimgayeon <yellow139@naver.com>
2019-07-10 02:24:29 +0900

    [ADD] add style of RankingPage

--- frontend/src/components/RankingPage.vue
+++ frontend/src/components/RankingPage.vue
@@ -1,29 +1,151 @@
 <template>
   <div class="bg">
     <div class="ranking">
-      <!--<div style="position: relative; top: 120px; left: 76px;">-->
-        <img src="../assets/ranking/ranking.png"></img>
+      <div><img class="rankingImg"  src="../assets/ranking/ranking.png"></div>
+
+      <div class="rankingBg">
+        <ul>
+          <li>
+            <span class="medal"><img class="medalImg" src="../assets/ranking/gold-medal.png"></span>
+            <span class="userImg"><img class="img" src="../assets/ranking/user.png"></span>
+            <span class="userName">User1</span>
+            <span class="score">1000</span>
+          </li>
+          <li>
+            <span class="medal"><img class="medalImg" src="../assets/ranking/silver-medal.png"></span>
+            <span class="userImg"><img class="img" src="../assets/ranking/user.png"></span>
+            <span class="userName">User2</span>
+            <span class="score">900</span>
+          </li>
+          <li>
+            <span class="medal"><img class="medalImg" src="../assets/ranking/bronze-medal.png"></span>
+            <span class="userImg"><img class="img" src="../assets/ranking/user.png"></span>
+            <span class="userName">User3</span>
+            <span class="score">700</span>
+          </li>
+          <li>
+            <span class="medal"><img class="medalImg" src="../assets/ranking/4th.png"></span>
+            <span class="userImg"><img class="img" src="../assets/ranking/user.png"></span>
+            <span class="userName">User4</span>
+            <span class="score">300</span>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <style>
-.bg {
-  background-image: url( "../assets/background/background.jpg" );
-  background-position: 50% 10%;
-  background-repeat:no-repeat;
-  background-size:cover;
-  height:100vh;
-  width:100vw;
-}
+  .bg {
+    background-image: url( "../assets/background/background.jpg" );
+    background-position: 50% 10%;
+    background-repeat:no-repeat;
+    background-size:cover;
+    height:100vh;
+    width:100vw;
+  }
 
-.bg .ranking{
-  top: 120px; 
-  left: 76px;
-  height: 20px;
-  weight: 30px;
-}
+  .ranking {
+    height: 100%;  
+    padding-bottom: 5px;
+    position: relative;
+    text-align: center;
+  }
+
+  .rankingImg {
+    height: 20%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding-bottom: 5px;
+    padding-top: 5px;
+    position: relative;
+    text-align: center;
+    width: 35%;
+  }
+
+  .img {
+    height: 80%;
+    width: auto;
+  }
+
+  .medalImg{
+    height: 90%;
+    width: auto;
+    margin: 0;
+    padding: 0;
+  }
+
+  ul{
+    position: relative;
+    width: 800px;
+    margin: 100px auto 0;
+    padding: 10px;
+    box-sizing: border-box;
+    background: rgb(85,140,47);
+    box-shadow: inset 0 0 15px rgba(0,0,0,.2);
+    border-radius: 5px;
+  }
+
+  ul li{
+    display: flex;
+    background: rgb(225,234,119);
+    border-radius: 5px;
+    box-sizing: border-box;
+    color: rgb(85,140,47);
+    transition: .5s;
+    height: 150px;
+    margin: 20px;
+  }
+
+  ul li span:nth-child(1){
+    padding: 0px 15px;
+    width: 150px;
+  }
+  ul li span:nth-child(2){
+    padding: 10px 20px;
+    margin: 15px 0;
+    width: 120px;
+  }
+  ul li span:nth-child(3){
+    padding: 10px 20px;
+    margin: 15px 0;
+    width: 230px;
+  }
+  ul li span:nth-child(4){
+    padding: 10px 20px;
+    margin: 15px 0;
+    width: 100px;
+  }
+
+  .rankingBg{
+    height: 80%;
+    margin:0;
+    padding: 0;
+    font-family: "Helvetica", "나눔 고딕", serif;
+  }
+
+  .userName{
+    bottom: 8px;
+    font-family: "Helvetica", "나눔 고딕", serif;
+    font-size: 70px;
+    font-weight: bold;
+    left: 16px;
+    position: relative;
+    text-align: center;
+    display: inline-block;
+  }
+
+  .score{
+    bottom: 8px;
+    color: rgb(85,140,47);
+    font-family: "Helvetica", "나눔 고딕", serif;
+    font-size: 70px;
+    font-weight: bold;
+    left: 16px;
+    position: relative;
+    text-align: center;
+    display: inline-block;
+  }
 </style>
 
 <script>
